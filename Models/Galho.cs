c43d7dd993cc66a3c9783db9cb19711ce56df4d1
felipe volgarini <felipe.volgarini@alfreddelivery.com>
2021-02-17 13:47:44 -0300

    Preparando a estrutura de dados e o formulário de entrada

---
+++ Models/Galho.cs
@@ -0,0 +1,14 @@
+﻿namespace Tarefa2.Models
+{
+    public class Galho
+    {
+        public int Numero { get; set; }
+        public Galho Proximo { get; set; }
+        
+        public Galho(int numero)
+        {
+            this.Numero = numero;
+            this.Proximo = null;            
+        }
+    }
+}
---
+++ Models/Raiz.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tarefa2.Models
+{
+    public class Raiz
+    {
+        public int Numero { get; set; }
+        public Galho Esquerda { get; set; }
+        public Galho Direita { get; set; }
+
+        public Raiz(int numero)
+        {
+            this.Numero = numero;
+            this.Esquerda = null;
+            this.Direita = null;
+        }
+    }
+}
